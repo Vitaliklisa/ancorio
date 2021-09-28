3317659d621bca99cd8be2f6394f10d3dbe02341
Vitaliklisa <vhomenko119@gmail.com>
2021-09-28 08:29:07 +0300

    New title and home page

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Анкорио-RP',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'I Анкорио-RP I Role Play Minecraft server'),
     );
   }
 }
